80e999d08e572d8f85bda3d71984126f7f0a45e6
Jamshid <murodovjamshid347@gmail.com>
2023-07-09 18:48:14 +0500

    test2

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-
-
-
 alert(" Izzatulloh aka shunday maqol bor:  Uyqingiz kelmasa,kitoblarni o'qing!!!")
 let book = +prompt("O'qilgan kitob sonini kiriting")
 while(isNaN(book) || book == 0){
     book = +prompt("Xatolik bor, qaytadan urinib ko'r ")
 }
 
-let num = " ta kitob"
-for (let i =1; i<=book; i++) {
- num = i + num
- console.log(num);
- num = " ta kitoblar"
+let num = 0
+for (let i = 1; i <= book; i++) {
+    num++
+    console.log(num + ' ' + "O'qilgan kitoblar");
+}
  alert("O'qilgan kitoblar  " + book + " ta")
- alert("Tashakkur o'qishda davom eting.")}
+ alert("Tashakkur o'qishda davom eting.")
